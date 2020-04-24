0436cc9b91a57384875e971e411020fd4cceeae4
name <email>
2020-04-25 00:45:56 +0300

    Adjust menu show/hide mechanic

--- Public/js/home.js
+++ Public/js/home.js
@@ -1,13 +1,25 @@
 function clickMenu() {
   var menuPopup = document.querySelector('#menu-popup');
   var opened = menuPopup.dataset.opened;
+  var menuButtonLink = document.querySelector('#menu-button').firstElementChild;
+  var accountMenuButton = document.getElementById("account-button");
   if (opened == "true") {
     menuPopup.style.display = "none";
+    menuButtonLink.style.backgroundImage = "url(../images/hamburger.svg)";
+    menuButtonLink.style.backgroundSize = "64px 64px";
+    menuButtonLink.style.width = "64px";
+    menuButtonLink.style.height = "64px";
     menuPopup.dataset.opened = "false";
+    accountMenuButton.style.display = "block";
     enableScroll();
   } else {
     menuPopup.style.display = "block";
+    menuButtonLink.style.backgroundImage = "url(../images/close.svg)";
+    menuButtonLink.style.backgroundSize = "26px 26px";
+    menuButtonLink.style.width = "26px";
+    menuButtonLink.style.height = "26px";
     menuPopup.dataset.opened = "true";
+    accountMenuButton.style.display = "none";
     disableScroll();
   }
 }
