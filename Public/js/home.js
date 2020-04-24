f7aad47ba95e72987a7b52d553d87aa265346c62
name <email>
2020-04-25 00:24:33 +0300

    Enable menu

--- Public/js/home.js
+++ Public/js/home.js
@@ -1,3 +1,17 @@
+function clickMenu() {
+  var menuPopup = document.querySelector('#menu-popup');
+  var opened = menuPopup.dataset.opened;
+  if (opened == "true") {
+    menuPopup.style.display = "none";
+    menuPopup.dataset.opened = "false";
+    enableScroll();
+  } else {
+    menuPopup.style.display = "block";
+    menuPopup.dataset.opened = "true";
+    disableScroll();
+  }
+}
+
 function clickAccount() {
   var accountPopup = document.querySelector('#account-popup');
   var haspopup = accountPopup.dataset.haspopup;
